278124cc6b32ea76599e6ac50bc6fdaa67db2758
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-01-01 23:13:05 +0100

    Create World

--- src/main/java/de/lifesimulator/Bootstrap.java
+++ src/main/java/de/lifesimulator/Bootstrap.java
@@ -1,8 +1,12 @@
 package de.lifesimulator;
 
+import de.lifesimulator.world.World;
+
+import java.io.IOException;
+
 public class Bootstrap {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        World.createWorld("Earth#1");
     }
 }
